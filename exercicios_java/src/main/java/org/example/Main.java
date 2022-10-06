1d93fa18b01feb3346d6dcb1328cc1b5963483f6
MuriloTischer <murilotischer@gmail.com>
2022-10-05 23:19:15 -0300

    #1 primeiro exercicio

---
+++ exercicios_java/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * @AUTHOR MURILO TADEU D. TISCHER ON ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
